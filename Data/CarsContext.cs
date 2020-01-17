53b7eda9f8ea9eaffbb6e601793a13008caeb8cb
dalexandre6 <47304497+dalexandre6@users.noreply.github.com>
2020-01-16 22:46:21 -0600

    .Net core 3.1 Mvc - 1
    .Net core 3.1 Mvc - 1

---
+++ Data/CarsContext.cs
@@ -0,0 +1,18 @@
+﻿using JunkCarsApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JunkCarsApp.Data
+{
+    public class CarsContext : DbContext 
+    {
+        public CarsContext (DbContextOptions<CarsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Car { get; set; } //Car is the object in the Model Class
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JunkCarsApp.Models
+{
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string City { get; set; }
+        public int Price { get; set; }
+    }
+}
